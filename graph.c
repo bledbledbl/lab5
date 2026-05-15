6b35380cda93d66dbf752ed6ef0795607fa3e724
Your Name <you@example.com>
2026-05-15 19:06:39 +0000

    new try

--- graph.c
+++ graph.c
@@ -59,6 +59,7 @@ int getWeight(Graph* g, const char* label1, const char* label2) {
     Edge * aux = list_first(edges);
     while(aux != NULL){
         if(strcmp(label1, label2) == 0) return aux->weight;
+        aux = list_next(edges);
     }
     // Si no existe el origen o terminamos de iterar sin encontrar el destino
     return -1; 
